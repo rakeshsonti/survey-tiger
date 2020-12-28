fcbdfc5518ac9ecbb20abd732e2ed11973a99ee4
rambhajansonti <rambhajansonti0597@gmail.com>
2020-12-28 16:42:13 +0530

    front-end  complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.PNG";
+import "./App.css";
+import { Button } from "reactstrap";
+import style from "./Mycss.module.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import CreateSurvey from "./components/CreateSurvey";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div className="App">
+         <Router>
+            <Switch>
+               <Route path="/create">
+                  <CreateSurvey />
+               </Route>
+               <Route path="/take">Take Survey</Route>
+               <Route path="/">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <Link to="/create">
+                     <Button className={style.surveymainbutton}>
+                        Create Survey
+                     </Button>
+                  </Link>
+                  <Link to="/take">
+                     <Button className={style.surveymainbutton}>
+                        Take Survey
+                     </Button>
+                  </Link>
+               </Route>
+            </Switch>
+         </Router>
+      </div>
+   );
 }
 
 export default App;
